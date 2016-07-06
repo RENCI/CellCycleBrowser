9ea739e9fdb28d3aecf82355ff263866181a8281
David Borland <borland@renci.org>
2016-07-06 13:00:36 -0400

    Use jumbotron instead of page-header for Header.

--- browser/app/components/Header.js
+++ browser/app/components/Header.js
@@ -2,7 +2,7 @@ var React = require("react");
 
 function Header(props) {
   return (
-    <div className="page-header col-sm-12 text-center">
+    <div className="jumbotron col-sm-12 text-center">
       <h1>{props.header}</h1>
     </div>
   );
