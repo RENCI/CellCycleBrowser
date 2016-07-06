9c4a82f61b3da713b5844bb00a00103901587652
David Borland <borland@renci.org>
2016-07-06 16:15:14 -0400

    Add dummy cell line select menu to header.

--- browser/app/components/Header.js
+++ browser/app/components/Header.js
@@ -1,11 +1,41 @@
 var React = require("react");
+var PropTypes = React.PropTypes;
+
+function option(d, i) {
+  return <option
+           key={i}
+           value={d.value}>
+             {d.name}
+         </option>
+}
+
+var minWidth = {
+  minWidth: 0,
+  width: "auto",
+  display: "inline"
+};
 
 function Header(props) {
   return (
     <div className="jumbotron col-sm-12 text-center">
       <h1>{props.header}</h1>
+      <div>
+        <h2>Cell line:</h2>
+        <select
+          className="form-control"
+          style={minWidth}
+          onChange={props.onChangeCellLine}>
+            {props.cellLines.map(option)}
+        </select>
+      </div>
     </div>
   );
 }
+
+Header.propTypes = {
+  header: PropTypes.string.isRequired,
+  cellLines: PropTypes.array.isRequired,
+  onChangeCellLine: PropTypes.func.isRequired
+};
 
 module.exports = Header;
--- browser/app/containers/HeaderContainer.js
+++ browser/app/containers/HeaderContainer.js
@@ -1,9 +1,34 @@
 var React = require("react");
 var Header = require("../components/Header");
 
+// Dummy information
+var cellLines = [
+  {value: "cellLine1", name: "Cell line 1"},
+  {value: "cellLine2", name: "Cell line 2"}
+];
+
 var HeaderContainer = React.createClass ({
+  getInitialState: function () {
+    return {
+      cellLine: ""
+    };
+  },
+  handleChangeCellLine: function (e) {
+    e.preventDefault();
+
+    this.setState({
+      cellLine: e.target.value
+    });
+
+    console.log(e.target.value);
+  },
   render: function () {
-    return <Header header="Cell Cycle Browser" />
+    return (
+      <Header
+        header="Cell Cycle Browser"
+        cellLines={cellLines}
+        onChangeCellLine={this.handleChangeCellLine} />
+    );
   }
 });
 
