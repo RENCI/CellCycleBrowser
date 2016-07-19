eddef920198720ca31073fb9ced4de23456f7aac
borland <borland@renci.org>
2016-07-19 14:13:48 -0400

    Add collapsible areas for cell data in each feature

--- browser/app/components/Feature.js
+++ browser/app/components/Feature.js
@@ -2,13 +2,31 @@ var React = require("react");
 var PropTypes = React.PropTypes;
 
 // TODO: Move to css file
-var divStyle = {
+var outerDivStyle = {
+  marginBottom: 20
+};
+
+var featureStyle = {
   backgroundColor: "white",
   borderColor: "#ddd",
   borderStyle: "solid",
   borderWidth: "2px",
-  borderRadius: 5
+  borderRadius: 5,
+  display: "block",
+  overflow: "auto"
 };
+
+var cellStyle = {
+  backgroundColor: "white",
+  borderColor: "#ddd",
+  borderStyle: "solid",
+  borderWidth: "1px",
+  borderRadius: 5
+}
+
+var buttonStyle = {
+  float: "left"
+}
 
 function Feature(props) {
   // Generate paragraphs with textual representations of cells
@@ -21,10 +39,30 @@ function Feature(props) {
     return <p key={i}>Cell {cell.name}: {values.toString()}</p>
   });
 
+  var id = props.name;
+
   return (
-    <div style={divStyle}>
-      <h3>Feature {props.name}</h3>
-      {cells}
+    <div style={outerDivStyle}>
+      <div style={featureStyle}>
+        <button
+          type="button"
+          className="btn btn-info"
+          data-toggle="collapse"
+          data-target={"#" + id}
+          style={buttonStyle}>
+            +
+        </button>
+        <span className="lead">
+            Feature {props.name}
+        </span>
+      </div>
+      <div
+        className="in"
+        id={id}
+        style={cellStyle}>
+          <p/>
+          {cells}
+      </div>
     </div>
   );
 }
