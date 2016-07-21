fe6afd47fd7ea3e06782429880de27d01ccc61e6
borland <borland@renci.org>
2016-07-20 20:01:18 -0400

    Add CellDataStore.js

---
+++ browser/app/stores/CellDataStore.js
@@ -0,0 +1,45 @@
+var AppDispatcher = require("../dispatcher/AppDispatcher");
+var EventEmitter = require("events").EventEmitter;
+var assign = require("object-assign");
+var Constants = require("../constants/Constants");
+
+var CHANGE_EVENT = "change";
+
+// Cell data
+var cellDataList = [];
+var cellData = {};
+
+var CellDataStore = assign({}, EventEmitter.prototype, {
+  emitChange: function () {
+    this.emit(CHANGE_EVENT);
+  },
+  addChangeListener: function (callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+  removeChangeListener: function (callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+  getCellDataList: function () {
+    return cellDataList;
+  },
+  getCellData: function () {
+    return map;
+  }
+});
+
+AppDispatcher.register(function (action) {
+  switch (action.actionType) {
+    case Constants.SELECT_CELL_DATA:
+      cellData = cellDataList[action.cellDataIndex];
+      CellDataStore.emitChange();
+      break;
+
+    case Constants.RECEIVE_DATA:
+      cellDataList = action.data.cellDataList;
+      cellData = maps.length > 0 ? cellDataList[0] : {};
+      CellDataStore.emitChange();
+      break;
+  }
+});
+
+module.exports = CellDataStore;
