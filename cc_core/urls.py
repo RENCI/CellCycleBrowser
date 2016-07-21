4243b04f60b26574712b5cec2c177b66685879b9
hyi <hongyi@renci.org>
2016-07-21 16:56:33 -0400

    get browser front-end and Django server back-end integrated

--- cc_core/urls.py
+++ cc_core/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^data/(?P<filename>[A-z0-9_.]+)$', views.serve_data, name='serve_data'),
 ]
--- cc_core/views.py
+++ cc_core/views.py
@@ -1,3 +1,5 @@
+import csv
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -6,6 +8,17 @@ from django.template import loader
 def index(request):
     template = loader.get_template('cc_core/index.html')
     context = {
-    'text_to_display': "Hello, world. You're at the Cell Cycle Browser index page.",
+    'text_to_display': "This Cell Cycle Browser allows exploration and simulation of the human cell cycle.",
     }
     return HttpResponse(template.render(context, request))
+
+def serve_data(request, filename, *args, **kwargs):
+    fp = open('data/'+filename, 'rb')
+    data = csv.reader(fp)
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="PCNA_53BP1_transpose.csv"'
+    writer = csv.writer(response)
+    for row in data:
+        writer.writerow(row)
+    
+    return response
