3cd0ee674abe8a2eb45346587fba497792bdcac5
David Borland <borland@renci.org>
2016-09-14 12:11:46 -0400

    Use ajax request to get cell profile list from server

--- browser/app/utils/WebAPIUtils.js
+++ browser/app/utils/WebAPIUtils.js
@@ -1,15 +1,47 @@
 var ServerActionCreators = require("../actions/ServerActionCreators");
 
-// Currently using localStorage as a proxy for XMLHttpRequest, or some other
-// method of getting data from the server.
+// Get a cookie for cross site request forgery (CSRF) protection
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // These HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
 
 module.exports = {
   getProfileList: function () {
-    setTimeout(function() {
-      var profileList = JSON.parse(localStorage.getItem("profileList"));
+    $.ajaxSetup({
+      beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+          xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }
+      }
+    });
 
-      ServerActionCreators.receiveProfileList(profileList);
-    }, 0);
+    $.ajax({
+      type: "POST",
+      url: '/get_profile_list/',
+      success: function (data) {
+        ServerActionCreators.receiveProfileList(data.profilelist);
+      },
+      error: function (xhr, textStatus, errorThrown) {
+        console.log(textStatus + ": " + errorThrown);
+      }
+    });
   },
   getProfile: function (profileKey) {
     setTimeout(function() {
