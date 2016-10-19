bb1296cc187ac542697f73598a20f505ce185940
David Borland <borland@renci.org>
2016-10-19 16:41:43 -0400

    Start adding groups for slider controls.

---
+++ client/app/components/SpeciesPhaseSliders.js
@@ -0,0 +1,43 @@
+var React = require("react");
+var SliderContainer = require("../containers/SliderContainer");
+var PropTypes = React.PropTypes;
+
+function SpeciesPhaseSliders(props) {
+  var sliders = props.species.map(function (species, i) {
+    function handleChange(value) {
+      props.onChange({
+        species: species,
+        value: value
+      });
+    };
+
+    return (
+      <SliderContainer
+        key={i}
+        label={species.name}
+        min={species.min}
+        max={species.max}
+        initialValue={species.value}
+        onChange={handleChange} />
+    );
+  });
+
+  return (
+    <div className="panel panel-default">
+      <div className="panel-heading">
+        <h3 className="panel-title">Species-phase interactions</h3>
+      </div>
+      <div className="panel-body">
+        {sliders}
+      </div>
+    </div>
+  );
+}
+
+SpeciesPhaseSliders.propTypes = {
+  species: PropTypes.arrayOf(PropTypes.object).isRequired,
+  phases: PropTypes.arrayOf(PropTypes.object).isReqiured,
+  onChange: PropTypes.func.isRequired
+};
+
+module.exports = SpeciesPhaseSliders;
--- client/app/containers/ControlsContainer.js
+++ client/app/containers/ControlsContainer.js
@@ -1,6 +1,7 @@
 var React = require("react");
 var ModelStore = require("../stores/ModelStore");
-var SliderContainer = require("./SliderContainer");
+var InitialValueSliders = require("../components/InitialValueSliders");
+var SpeciesPhaseSliders = require("../components/SpeciesPhaseSliders");
 var WebAPIUtils = require("../utils/WebAPIUtils");
 
 function getStateFromStore() {
@@ -25,37 +26,36 @@ var ControlsContainer = React.createClass ({
     this.setState(getStateFromStore());
   },
   handleSliderChange: function (data) {
+    console.log(data);
+
     WebAPIUtils.sendParameter({
       species: data.species.name,
       value: data.value
     });
   },
+  handleButtonClick: function (data) {
+    console.log("CLICK!");
+  },
   render: function () {
     if (!this.state.model) return null;
-
-    var sliders = this.state.model.species.map(function (species, i) {
-      function handleChange(value) {
-        this.handleSliderChange({
-          species: species,
-          value: value
-        });
-      };
-
-      return (
-        <SliderContainer
-          key={i}
-          label={species.name}
-          min={species.min}
-          max={species.max}
-          initialValue={species.value}
-          onChange={handleChange.bind(this)} />
-      );
-    }.bind(this));
 
     return (
       <div>
         <h2>Controls</h2>
-        {sliders}
+        <InitialValueSliders
+          species={this.state.model.species}
+          onChange={this.handleSliderChange} />
+        <SpeciesPhaseSliders
+          species={this.state.model.species}
+          phases={this.state.model.phases}
+          onChange={this.handleSliderChange} />
+        <button
+          type="button"
+          className="btn btn-info"
+          style={{width: "100%"}}
+          onClick={this.handleButtonClick}>
+            Run Model
+        </button>
       </div>
     );
   }
