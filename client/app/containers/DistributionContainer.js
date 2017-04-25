0dc152b3d767b61e9f9304d7542b765d6fb37da2
David Borland <borland@renci.org>
2017-04-25 15:29:35 -0400

    Move cell generating code from Distribution visualization to container so only called when model is changed, fix bug by adding class to points in visualization

--- client/app/containers/DistributionContainer.js
+++ client/app/containers/DistributionContainer.js
@@ -15,15 +15,101 @@ var divStyle = {
 
 function getStateFromModelStore() {
   return {
-    model: ModelStore.getModel()
+    cells: createCells(ModelStore.getModel().reactions)
   };
+}
+
+function createCells(reactions) {
+  if (!reactions) return [];
+
+  // Number of cells
+  var n = 5000;
+
+  // Normalized random distribution function
+  var randn = d3.randomNormal();
+
+  // Create phase objects with probabilities
+  var phases = reactions.map(function(d) {
+    return {
+      name: d.reactant.replace("_end", ""),
+      kf: d.kf,
+      p: 1 / d.kf
+    };
+  });
+
+  // Number of S subphases
+  var numS = phases.filter(function(d) {
+    return d.name.indexOf("S") !== - 1;
+  }).length;
+
+  // Normalize probabilities
+  var pSum = d3.sum(phases, function(d) { return d.p; });
+
+  phases.forEach(function(d) {
+    d.p /= pSum;
+  });
+
+  // Create cumulative probabilities for sampling
+  phases.reduce(function(p, c) {
+    return c.cumP = c.p + p;
+  }, 0);
+
+  return d3.range(0, n).map(function() {
+    var cell = {},
+        p = Math.random();
+
+    // Determine phase for cell
+    for (var i = 0; i < phases.length; i++) {
+      if (p < phases[i].cumP) {
+        cell.phase = phases[i];
+        break;
+      }
+    }
+
+    // Position the cell based on phase
+    var name = cell.phase.name;
+
+    if (name.indexOf("G1") !== -1) {
+      cell.x = 1;
+      cell.y = 1;
+
+      // Random offset
+      cell.x += 0.05 * randn();
+      cell.y += 0.05 * randn();
+    }
+    else if (name.indexOf("G2") !== -1) {
+      cell.x = 2;
+      cell.y = 1;
+
+      // Random offset
+      cell.x += 0.05 * randn();
+      cell.y += 0.05 * randn();
+    }
+    else if (name.indexOf("S") !== -1) {
+      // Get S subphase index
+      var sIndex = +name.substr(name.indexOf("_") + 1) - 1;
+
+//        cell.x = 1 + 0.25 + sIndex / numS;
+      cell.x = 1.5;
+      cell.y = 2;
+
+      // Random offset
+      var r = 0.25 * randn();
+      cell.x += r;
+      cell.y += -Math.abs(r) + 0.25 * randn();
+    }
+    else {
+      console.log("Phase not handled");
+      cell.x = cell.y = 0;
+    }
+
+    return cell;
+  });
 }
 
 var DistributionContainer = React.createClass ({
   getInitialState: function () {
-    return {
-      model: ModelStore.getModel()
-    };
+    return getStateFromModelStore();
   },
   componentDidMount: function() {
     // Create visualiztion function
@@ -51,10 +137,8 @@ var DistributionContainer = React.createClass ({
     this.resize();
   },
   drawVisualization: function (state) {
-    if (!state.model.reactions) return;
-
     d3.select(this.getNode())
-        .datum(state.model.reactions)
+        .datum(state.cells)
         .call(this.distribution);
   },
   resize: function () {
--- client/app/visualizations/Distribution.js
+++ client/app/visualizations/Distribution.js
@@ -12,7 +12,6 @@ module.exports = function() {
 
       // Data
       data = [],
-      cells = [],
       contours = [],
 
       // Scales
@@ -48,94 +47,7 @@ module.exports = function() {
 
       svg = svgEnter.merge(svg);
 
-      createCells();
       draw();
-    });
-  }
-
-  function createCells() {
-    // Number of cells
-    var n = 5000;
-
-    // Normalized random distribution function
-    var randn = d3.randomNormal();
-
-    // Create phase objects with probabilities
-    var phases = data.map(function(d) {
-      return {
-        name: d.reactant.replace("_end", ""),
-        kf: d.kf,
-        p: 1 / d.kf
-      };
-    });
-
-    // Number of S subphases
-    var numS = phases.filter(function(d) {
-      return d.name.indexOf("S") !== - 1;
-    }).length;
-
-    // Normalize probabilities
-    var pSum = d3.sum(phases, function(d) { return d.p; });
-
-    phases.forEach(function(d) {
-      d.p /= pSum;
-    });
-
-    // Create cumulative probabilities for sampling
-    phases.reduce(function(p, c) {
-      return c.cumP = c.p + p;
-    }, 0);
-
-    cells = d3.range(0, n).map(function() {
-      var cell = {},
-          p = Math.random();
-
-      // Determine phase for cell
-      for (var i = 0; i < phases.length; i++) {
-        if (p < phases[i].cumP) {
-          cell.phase = phases[i];
-          break;
-        }
-      }
-
-      // Position the cell based on phase
-      var name = cell.phase.name;
-
-      if (name.indexOf("G1") !== -1) {
-        cell.x = 1;
-        cell.y = 1;
-
-        // Random offset
-        cell.x += 0.05 * randn();
-        cell.y += 0.05 * randn();
-      }
-      else if (name.indexOf("G2") !== -1) {
-        cell.x = 2;
-        cell.y = 1;
-
-        // Random offset
-        cell.x += 0.05 * randn();
-        cell.y += 0.05 * randn();
-      }
-      else if (name.indexOf("S") !== -1) {
-        // Get S subphase index
-        var sIndex = +name.substr(name.indexOf("_") + 1) - 1;
-
-//        cell.x = 1 + 0.25 + sIndex / numS;
-        cell.x = 1.5;
-        cell.y = 2;
-
-        // Random offset
-        var r = 0.25 * randn();
-        cell.x += r;
-        cell.y += -Math.abs(r) + 0.25 * randn();
-      }
-      else {
-        console.log("Phase not handled");
-        cell.x = cell.y = 0;
-      }
-
-      return cell;
     });
   }
 
@@ -146,7 +58,7 @@ module.exports = function() {
       .y(function(d) { return yScale(d.y); })
       .size([innerWidth(), innerHeight()])
   //      .bandwidth(40)
-      (cells);
+      (data);
   }
 
   function draw() {
@@ -222,10 +134,11 @@ module.exports = function() {
 
       // Bind cell data
       var point = svg.select(".points").selectAll(".point")
-          .data(cells);
+          .data(data);
 
       // Enter + update
       point.enter().append("circle")
+          .attr("class", "point")
           .attr("r", 1)
         .merge(point)
           .attr("cx", function(d) { return xScale(d.x); })
