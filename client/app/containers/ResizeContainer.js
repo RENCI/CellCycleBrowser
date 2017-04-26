b1c2117c483c332d54fcdfdcac0fc7370dbb0c8f
David Borland <borland@renci.org>
2017-04-26 10:02:36 -0400

    Add ResizeContainer to handle resizing due to scrollbar addition/removal

---
+++ client/app/containers/ResizeContainer.js
@@ -0,0 +1,35 @@
+// Throw a window resize event whenever this iframe changes width, which
+// accounts for scrollbars being added/removed.
+// Adapted from: https://gist.github.com/AdamMcCormick/d5f718d2e9569acdf7def25e8266bb2a
+
+var React = require("react");
+
+var style = {
+  height: 0,
+  margin: 0,
+  padding: 0,
+  overflow: "hidden",
+  borderWidth: 0,
+  position: "absolute",
+  backgroundColor: "transparent",
+  width: "100%"
+};
+
+var ResizeContainer = React.createClass({
+  componentDidMount() {
+    this.refs.frame.contentWindow.addEventListener("resize", this.onResize, false);
+  },
+  componentWillUnmount() {
+    this.refs.frame.contentWindow.removeEventListener("resize", this.onResize);
+  },
+  onResize() {
+    try {
+      window.dispatchEvent(new UIEvent("resize"));
+    } catch (d) {}
+  },
+  render() {
+    return <iframe ref="frame" style={style} />
+  }
+});
+
+module.exports = ResizeContainer;
