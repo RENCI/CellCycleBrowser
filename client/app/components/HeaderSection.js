5e78182d3198303104598081f5624d17c1e43726
David Borland <borland@renci.org>
2017-06-08 16:07:59 -0400

    Stack unc and renci logos

--- client/app/components/HeaderSection.js
+++ client/app/components/HeaderSection.js
@@ -8,7 +8,7 @@ function HeaderSection() {
         <div className="media-left media-middle text-center" style={{width:"25%"}}>
           <img
             src="/static/cc_core/images/ccb_logo_alpha_cropped.png"
-            style={{height: "100px"}} />
+            style={{height: 100}} />
         </div>
         <div className="media-body media-middle text-center">
           <Header header="Cell Cycle Browser" />
@@ -16,17 +16,23 @@ function HeaderSection() {
           <a style={{marginRight: 20}} href="/team/" target="_blank">Team</a>
           <a href="/help/" target="_blank">Help</a>
         </div>
-        <div className="media-right media-middle text-center" style={{width:"25%"}}>
-          <a href="http://www.unc.edu/">
-            <img
-              src="/static/cc_core/images/medium_blue_450px.png"
-              style={{height: "70px", float: "left"}} />
-          </a>
-          <a href="http://renci.org/">
-            <img
-              src="/static/cc_core/images/RENCI-Official-Logo-No-Tagline-cropped-alpha.png"
-              style={{width: "80px", marginTop: 5, marginRight: 20}} />
-          </a>
+        <div
+          className="media-right media-middle text-center"
+          style={{width:"25%", height: 100}}>
+          <div>
+            <a href="http://www.unc.edu/">
+              <img
+                src="/static/cc_core/images/medium_blue_450px.png"
+                style={{height: 60}} />
+            </a>
+          </div>
+          <div>
+            <a href="http://renci.org/">
+              <img
+                src="/static/cc_core/images/RENCI-Official-Logo-No-Tagline-cropped-alpha.png"
+                style={{height: 40}} />
+            </a>
+          </div>
         </div>
       </div>
     </div>
