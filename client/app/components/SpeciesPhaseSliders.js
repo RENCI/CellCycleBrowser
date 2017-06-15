4ed588e6ccbb248e2a5946f2d1f8685bec4bbf97
borland <borland@renci.org>
2017-06-15 12:19:11 -0400

    Temporary fix for resizing value sliders when tab changes

--- client/app/components/SpeciesPhaseSliders.js
+++ client/app/components/SpeciesPhaseSliders.js
@@ -2,6 +2,7 @@ var React = require("react");
 var PropTypes = React.PropTypes;
 var Collapsible = require("../components/Collapsible");
 var ValueSliderContainer = require("../containers/ValueSliderContainer");
+var ViewActionCreators = require("../actions/ViewActionCreators");
 var d3 = require("d3");
 var d3ScaleChromatic = require("d3-scale-chromatic");
 
@@ -45,19 +46,34 @@ function SpeciesPhaseSliders(props) {
     });
 
     var tabId = "speciesPhase" + phase;
-    var active = (props.activePhase === "" && i === 0) || phase === props.activePhase;
+
+    // XXX: Select a tab index if phase is currently not selected to force
+    // a render of tabs to size sliders correctly. Should probably keep this
+    // logic separate and add a container to keep the tab state.
+    var activeIndex = +props.activePhase;
+    var active = (props.activePhase === "" && i === 0) ||
+                 (i === activeIndex) ||
+                 phase === props.activePhase;
+
+    function onClick() {
+      if (props.activePhase === "" || !isNaN(activeIndex)) {
+        ViewActionCreators.selectPhase(i + "");
+      }
+      else {
+        ViewActionCreators.selectPhase(phase);
+      }
+    }
 
     return {
       tab: (
         <li
-          className={"nav" + (active ? " active" : "")} key={i}>
+          key={i}
+          className={"nav" + (active ? " active" : "")}
+          onClick={onClick}>
             <a
               href={"#" + tabId}
               data-toggle="tab"
-              style={{
-                backgroundColor: colorScale(phase),
-                color: "black"
-              }}>
+              style={{backgroundColor: colorScale(phase), color: "black"}}>
                 {phase}
             </a>
         </li>
--- client/app/components/SpeciesSpeciesSliders.js
+++ client/app/components/SpeciesSpeciesSliders.js
@@ -2,6 +2,7 @@ var React = require("react");
 var PropTypes = React.PropTypes;
 var Collapsible = require("../components/Collapsible");
 var ValueSliderContainer = require("../containers/ValueSliderContainer");
+var ViewActionCreators = require("../actions/ViewActionCreators");
 var d3 = require("d3");
 var d3ScaleChromatic = require("d3-scale-chromatic");
 
@@ -77,20 +78,36 @@ function SpeciesSpeciesSliders(props) {
     });
 
     var tabId = "speciesSpecies" + phase;
-    var active = (props.activePhase === "" && i === 0) || phase === props.activePhase;
+
+    // XXX: Select a phase index if phase is currently not selected to force
+    // a render of tabs to size sliders correctly. Should probably keep this
+    // logic separate and add a container to keep the tab state.
+    var activeIndex = +props.activePhase;
+    var active = (props.activePhase === "" && i === 0) ||
+                 (i === activeIndex) ||
+                 phase === props.activePhase;
+
+    function onClick() {
+      if (props.activePhase === "" || !isNaN(activeIndex)) {
+        ViewActionCreators.selectPhase(i + "");
+      }
+      else {
+        ViewActionCreators.selectPhase(phase);
+      }
+    }
 
     return {
       tab: (
-        <li className={"nav" + (active ? " active" : "")} key={i}>
-          <a
-            href={"#" + tabId}
-            data-toggle="tab"
-            style={{
-              backgroundColor: colorScale(phase),
-              color: "black"
-            }}>
-              {phase}
-          </a>
+        <li
+          key={i}
+          className={"nav" + (active ? " active" : "")}
+          onClick={onClick}>
+            <a
+              href={"#" + tabId}
+              data-toggle="tab"
+              style={{backgroundColor: colorScale(phase), color: "black"}}>
+                {phase}
+            </a>
         </li>
       ),
       content: (
