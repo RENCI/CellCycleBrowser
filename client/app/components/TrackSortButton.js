b020a1346f0f6eae864008ba2d6b5bf4ead22835
David Borland <borland@renci.org>
2017-08-29 10:37:03 -0400

    Change sort icon

--- client/app/components/TrackSortButton.js
+++ client/app/components/TrackSortButton.js
@@ -19,7 +19,7 @@ function TrackSortButton(props) {
       title={props.toolTip}
       onClick={props.onClick}>
         <span
-          className="glyphicon glyphicon-arrow-down"
+          className="glyphicon glyphicon-sort-by-attributes"
           style={iconStyle} />
     </label>
   );
