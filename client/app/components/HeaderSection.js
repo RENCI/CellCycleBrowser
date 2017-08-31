de3661e199d99c622a5ea4dfda288df36026716d
David Borland <borland@renci.org>
2017-08-31 14:34:33 -0400

    Make header stack when small

--- client/app/components/HeaderSection.js
+++ client/app/components/HeaderSection.js
@@ -4,23 +4,20 @@ var Header = require("./Header");
 function HeaderSection() {
   return (
     <div className="row well">
-      <div className="media">
-        <div className="media-left media-middle text-center" style={{width:"25%"}}>
+        <div className="col-sm-3 text-center">
           <a href="http://www.med.unc.edu/genetics/purvislab">
             <img
               src="/static/cc_core/images/ccb_logo_alpha_cropped.png"
               style={{height: 100}} />
           </a>
         </div>
-        <div className="media-body media-middle text-center">
+        <div className="col-sm-6 text-center">
           <Header header="Cell Cycle Browser" />
           <a style={{marginRight: 20}} href="/about/" target="_blank">About</a>
           <a style={{marginRight: 20}} href="/team/" target="_blank">Team</a>
           <a href="/help/" target="_blank">Help</a>
         </div>
-        <div
-          className="media-right media-middle text-center"
-          style={{width:"25%", height: 100}}>
+        <div className="col-sm-3 text-center">
           <div>
             <a href="http://www.unc.edu/">
               <img
@@ -36,9 +33,45 @@ function HeaderSection() {
             </a>
           </div>
         </div>
+    </div>
+  );
+/*
+  return (
+    <div className="row well">
+      <div className="media">
+        <div className="media-left media-middle text-center" style={{width: "25%"}}>
+          <a href="http://www.med.unc.edu/genetics/purvislab">
+            <img
+              src="/static/cc_core/images/ccb_logo_alpha_cropped.png"
+              style={{maxHeight: 100, width: "auto"}} />
+          </a>
+        </div>
+        <div className="media-body media-middle text-center">
+          <Header header="Cell Cycle Browser" />
+          <a style={{marginRight: 20}} href="/about/" target="_blank">About</a>
+          <a style={{marginRight: 20}} href="/team/" target="_blank">Team</a>
+          <a href="/help/" target="_blank">Help</a>
+        </div>
+        <div className="media-right media-middle text-center" style={{width: "25%"}}>
+          <div style={{height: "60%"}}>
+            <a href="http://www.unc.edu/">
+              <img
+                src="/static/cc_core/images/medium_blue_450px.png"
+                style={{maxHeight: 60, width: "auto", minWidth: 10}} />
+            </a>
+          </div>
+          <div>
+            <a href="http://renci.org/">
+              <img
+                src="/static/cc_core/images/RENCI-Official-Logo-No-Tagline-cropped-alpha.png"
+                style={{maxHeight: 40, width: "auto", minWidth: 10}} />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
+*/
 }
 
 module.exports = HeaderSection;
