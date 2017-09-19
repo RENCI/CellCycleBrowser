891f3b3a89ededa766168206b3f5f5b9c16cd663
David Borland <borland@renci.org>
2017-09-19 14:50:19 -0400

    Editing information popups

--- client/app/components/FlowCytometryInformation.js
+++ client/app/components/FlowCytometryInformation.js
@@ -5,7 +5,7 @@ function FlowCytometryInformation() {
   return (
     <InformationWrapper>
       <h4>Cell Cycle Analysis Plot</h4>
-      <p>A simulated cell cycle analysis plot is shown for each data <b>source</b> represented in the <i>Data Browser</i> that contains phase information. Each data point represents an indivual cell, with phase generated probabalistically based on the average phase lengths for that data <b>source</b>.</p>
+      <p>A simulated flow cytometry plot is shown for each data <b>source</b> represented in the <i>Data Browser</i> that contains phase information. Each data point represents an individual cell, with phase generated probabilistically based on the average phase lengths for that data <b>source</b>.</p>
       <p>Each cell is colored based on the density of cells around it, with the color map matching the <i>Source color</i> icons for each track in the <i>Data Browser</i></p>
     </InformationWrapper>
   );
